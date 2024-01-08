a4f58ee87020ccfbdec1bcba3ab89d68383df25a
wils <tsikhaviwils@gmail.com>
2024-01-08 11:15:51 +0300

    changes

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -72,7 +72,7 @@ function Section() {
 
               <Link
                 href="#"
-                className="mt-8 inline-block rounded bg-allports-600 px-12 py-3 text-sm font-medium text-black transition hover:bg-allports-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                className="mt-8 inline-block rounded bg-allports-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-allports-700 focus:outline-none focus:ring focus:ring-yellow-400"
               >
                 Get Started Today
               </Link>
@@ -85,7 +85,7 @@ function Section() {
       </section>
 
       <section className="pt-20">
-        <Carousel />
+        
       </section>
 
       <section className="pt-20">
@@ -98,13 +98,15 @@ function Carousel() {
   return (
     <div class="group relative m-10 h-96 w-96 overflow-hidden rounded-lg shadow-md">
       <div class="absolute left-0 top-0 h-full w-full transition-all duration-300 ease-in-out group-hover:-top-96">
-        <img
+        <Image
           class="h-4/6 w-full object-cover"
-          src="https://images.unsplash.com/photo-1660648127319-3f071bbac474?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          src="/Images/unsplash4.jpg"
           alt=""
+          width={720}
+          height={720}
         />
         <h1 class="mt-4 px-4 text-center font-serif text-xl font-semibold text-rose-500">
-          Portugal&apos; Hot Baloon Festival
+          Esthete Cleaning
         </h1>
         <p class="mt-1 px-4 text-center text-gray-600">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, saepe.
@@ -113,7 +115,7 @@ function Carousel() {
 
       <div class="absolute left-0 -bottom-96 flex h-full w-full flex-col justify-center transition-all duration-300 ease-in-out group-hover:bottom-0">
         <h1 class="mb-2 px-8 text-center font-serif text-xl font-semibold text-rose-500">
-          Portugal&apos; Hot Baloon Festival
+          Esthete Cleaning
         </h1>
         <p class="px-8 text-center">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero deserunt quisquam aperiam, voluptas esse
